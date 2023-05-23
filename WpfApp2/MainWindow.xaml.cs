904c4d729927163c07fcda804d2490bc6b4972f0
marinam34 <119538174+marinam34@users.noreply.github.com>
2023-05-24 00:50:11 +0300

    Update MainWindow.xaml.cs

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+using ClassLibrary2;
+using Microsoft.Win32;
+using System;
 using System.Windows;
 using ViewModel;
 
@@ -19,6 +21,25 @@ namespace WpfApp2
         {
             errorBar.Text = message;
             errorBar.Visibility = Visibility.Visible;
+        }
+
+        public string? ReportSaveFile()
+        {
+            SaveFileDialog files = new SaveFileDialog();
+            if (files.ShowDialog() == true)
+            {
+                return files.FileName;
+            }
+            return null;
+        }
+        public string? ReportOpenFile()
+        {
+            OpenFileDialog files = new OpenFileDialog();
+            if (files.ShowDialog() == true)
+            {
+                return files.FileName;
+            }
+            return null;
         }
     }
 }
